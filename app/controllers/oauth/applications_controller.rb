1ef6c517618e8c0b017002e23ff509cf29abb3b0
Simon Bonnard <smn.bonnard@gmail.com>
2013-10-16 12:14:32 +0200

    [wip] override default applications controller

--- app/controllers/oauth/applications_controller.rb
+++ app/controllers/oauth/applications_controller.rb
@@ -1,11 +1,11 @@
 class Oauth::ApplicationsController < Doorkeeper::ApplicationsController
   respond_to :html
-
   before_filter :authenticate_user!
 
-  def index
-    @applications = current_user.oauth_applications
-  end
+#  def index
+#    @applications = current_user.applications
+#    respond_with @applications
+#  end
 
   def new
     @application = Doorkeeper::Application.new
@@ -23,37 +23,35 @@ class Oauth::ApplicationsController < Doorkeeper::ApplicationsController
     end
   end
 
-  def show
-    @application = current_user.oauth_applications.find(params[:id])
-  end
-
-  def edit
-    @application = current_user.oauth_applications.find(params[:id])
-  end
-
-  def update
-    @application = current_user.oauth_applications.find(params[:id])
-    if @application.update_attributes(application_params)
-      flash[:notice] = I18n.t(:notice, :scope => [:doorkeeper, :flash, :applications, :update])
-      respond_with [:oauth, @application]
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @application = current_user.oauth_applications.find(params[:id])
-    flash[:notice] = I18n.t(:notice, :scope => [:doorkeeper, :flash, :applications, :destroy]) if @application.destroy
-    redirect_to oauth_applications_url
-  end
-
+#  def show
+#    @application = current_user.applications.find(params[:id])
+#    respond_with @application
+#  end
+#
+#  def edit
+#    @application = current_user.applications.find(params[:id])
+#  end
+#
+#  def update
+#    @application = current_user.applications.find(params[:id])
+#    if @application.update_attributes(application_params)
+#      flash[:notice] = I18n.t(:notice, :scope => [:doorkeeper, :flash, :applications, :update])
+#      respond_with [:oauth, @application]
+#    else
+#      render :edit
+#    end
+#  end
+#
+#  def destroy
+#    @application = current_user.applications.find(params[:id])
+#    flash[:notice] = I18n.t(:notice, :scope => [:doorkeeper, :flash, :applications, :destroy]) if @application.destroy
+#    redirect_to oauth_applications_url
+#  end
+#
   private
 
-  def application_params
-    if params.respond_to?(:permit)
-      params.require(:application).permit(:name, :redirect_uri)
-    else
-      params[:application].slice(:name, :redirect_uri) rescue nil
-    end
+  def current_user
+    @current_user ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
   end
+
 end
